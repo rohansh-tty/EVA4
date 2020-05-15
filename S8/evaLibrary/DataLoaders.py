4cd3052f0184e4d91fcf39ceb4fca94c0646cf29
Rohan Shetty <40785407+Gilf641@users.noreply.github.com>
2020-05-15 23:12:31 +0530

    Update DataLoaders.py

--- S8/evaLibrary/DataLoaders.py
+++ S8/evaLibrary/DataLoaders.py
@@ -1,10 +1,9 @@
 import torch
 
-
 class DataLoaders:
   def __init__(self, batch_size=128, shuffle=True, num_workers=4, pin_memory=True, seed=1):
     use_cuda = torch.cuda.is_available()
-    device = torch.device('cuda' if use_cuda else 'cpu')
+    device = torch.device('cuda' if use_cuda else 'cpu')  # set device to cuda
 
     if use_cuda:
       torch.manual_seed(seed)
