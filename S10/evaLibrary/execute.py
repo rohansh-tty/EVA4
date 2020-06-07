c5f1b5d5e515a13f361e99a30c83283420f6a8d1
Rohan Shetty <40785407+Gilf641@users.noreply.github.com>
2020-06-07 06:24:30 +0530

    Update Execute

--- S10/evaLibrary/execute.py
+++ S10/evaLibrary/execute.py
@@ -5,7 +5,13 @@ import torch
 
 
 class Test_Train():
-  def __init__(self):
+  def __init__(self, model, device, optimizer, criterion):
+
+
+    self.model = model
+    self.device = device
+    self.optimizer = optimizer
+    self.criterion = criterion
 
 # # This is to hold all the values and plot some graphs to extract few good insights.
     self.train_losses = []
@@ -17,49 +23,50 @@ class Test_Train():
     # when the test loss becomes min I will save the particular model
 
 
-  def train(self, model, device, trainloader, optimizer, epoch,scheduler, criterion, L1lambda=None):
-    model.train()    # prepare model for training
+  def train(self, trainloader, epoch, L1lambda=None):
+    self.model.train()    # prepare model for training
     pbar = tqdm(trainloader)
     correct = 0
     processed = 0
     for batch_idx, (data, target) in enumerate(pbar): # passing on data & target values to device
-      data, target = data.to(device), target.to(device)
-      optimizer.zero_grad()    # clear the gradients of all optimized variables
-      
+      data, target = data.to(self.device), target.to(self.device)
+      self.optimizer.zero_grad()    # clear the gradients of all optimized variables
+
       # Predict
-      y_pred = model(data)   # forward pass
+      y_pred = self.model(data)   # forward pass
 
       # Calculate loss
-      loss = criterion(y_pred, target)
+      c_loss = self.criterion(y_pred, target)
 
       #Implementing L1 Regularization
       if L1lambda:
         with torch.enable_grad():
           l1_loss = 0.
-          for param in model.parameters():
+          for param in self.model.parameters():
             l1_loss += torch.sum(param.abs())
-          loss += L1lambda * l1_loss
+          loss = c_loss +  (L1lambda * l1_loss)
 
-      self.train_losses.append(loss)
+
 
       # Backpropagation
       loss.backward()   # backward pass: compute gradient of the loss with respect to model parameters
-      optimizer.step()   # perform a single optimization step (parameter update)
+      self.optimizer.step()   # perform a single optimization step (parameter update)
 
       # Update pbar-tqdm
-    
+
       pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
       correct += pred.eq(target.view_as(pred)).sum().item()
       processed += len(data)
 
       pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
-      self.train_acc.append(100*correct/processed)
-      scheduler.step()
-      self.train_epoch_end.append(self.train_acc[-1])
+    self.train_acc.append(100*correct/processed)
+      # scheduler.step()
+    self.train_epoch_end.append(self.train_acc[-1])
+    self.train_losses.append(loss)
 
 
-  def test(self, model, device, testloader,filename, criterion, correct_samples, correctLabels, incorrect_samples):
-      model.eval()  # prep model for evaluation
+  def test(self, testloader, filename, correct_samples, correctLabels, incorrect_samples):
+      self.model.eval()  # prep model for evaluation
       test_loss = 0
       correct = 0
 
@@ -67,22 +74,23 @@ class Test_Train():
           for data, target in testloader:
 
             img_batch = data # this is done to store data
-            data, target = data.to(device), target.to(device)
+            data, target = data.to(self.device), target.to(self.device)
 
             # forward pass: compute predicted outputs by passing inputs to the model
-            output = model(data)  
+            output = self.model(data)
 
             # sum up batch loss
-            # test_loss += F.nll_loss(output, target, reduction='sum').item()  
-            test_loss = criterion(output, target).item()
+            # test_loss += F.nll_loss(output, target, reduction='sum').item()
+            test_loss = self.criterion(output, target).item()
 
 
             # get the index of the max log-probability
-            pred = output.argmax(dim=1, keepdim=True)  
-            correct += pred.eq(target.view_as(pred)).sum().item()  
-            
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
             # storing the entire result data as binary
             result = pred.eq(target.view_as(pred))
+            # scheduler.step()
 
             # This is to extract incorrect samples/misclassified images
             if len(incorrect_samples) < 25:
@@ -97,22 +105,23 @@ class Test_Train():
                 if list(result)[i]:
                   correct_samples.append({'prediction': list(pred)[i], 'label': list(target.view_as(pred))[i],'image': img_batch[i]})
                   correctLabels.append(list(target.view_as(pred))[i]) # this is for gradcam
-                
-      self.test_losses.append(test_loss)      
+
+
 
       # save model if validation loss has decreased
       if test_loss <= self.test_loss_min:
           print('Validation loss has  decreased ({:.4f} --> {:.4f}).  Saving model ...'.format(self.test_loss_min,test_loss ))
-          torch.save(model.state_dict(), filename)
+          torch.save(self.model.state_dict(), filename)
           self.test_loss_min = test_loss
 
 
       print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
           test_loss, correct, len(testloader.dataset),
           100. * correct / len(testloader.dataset)))
-    
-      self.test_acc.append(100. * correct / len(testloader.dataset))
 
+
+      self.test_acc.append(100. * correct / len(testloader.dataset))
+      self.test_losses.append(test_loss)
 
 
 
