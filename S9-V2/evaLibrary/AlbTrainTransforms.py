cdea464429f96961943f69324a07a94c45b93d8c
RohanShetty <shettyrohan641@yahoo.com>
2020-06-22 21:31:34 +0530

    Train Transforms

--- S9-V2/evaLibrary/AlbTrainTransforms.py
+++ S9-V2/evaLibrary/AlbTrainTransforms.py
@@ -1,20 +1,19 @@
-from albumentations import Compose, RandomCrop, Normalize, HorizontalFlip, VerticalFlip, Resize, Rotate,  Cutout, Flip
+from albumentations import Compose, RandomCrop, Normalize, Resize, Rotate, Cutout, PadIfNeeded, RandomCrop, Flip
 from albumentations.pytorch import ToTensor
 import numpy as np
 
 
 class train_transforms():
-
+    
     def __init__(self):
-        self.albTrainTransforms = Compose([  # Resize(256, 256),
-            Rotate((-10.0, 10.0)),
-            Flip(p=0.5),
-            Cutout(num_holes = 1, max_h_size = 8, max_w_size = 8, p = 1.0),
+        self.albTrainTransforms = Compose([ 
+            PadIfNeeded(min_height = 36, min_width = 36, border_mode = 0, p = 1.0),
+            RandomCrop(height = 32, width = 32, p = 1.0),
+            Rotate((-15.0,15.0), p=0.7),
+            Cutout(num_holes = 2, max_h_size = 8, max_w_size = 8, p = 1.0),
             Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
             ToTensor()
         ])
-
-    print("REQUIRED LIBRARIES LOADED...")
 
     def __call__(self, img):
         img = np.array(img)
