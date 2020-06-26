8702bfbb5228f782866339fb124ab28ff8be0430
RohanShetty <shettyrohan641@yahoo.com>
2020-06-26 18:14:37 +0530

    TinyImageNet Custom Dataset

--- S12/evaLibrary/TinyImageNetDataset.py
+++ S12/evaLibrary/TinyImageNetDataset.py
@@ -1,175 +1,114 @@
-import os
+from torch.utils.data import Dataset, random_split
+from PIL import Image
 import numpy as np
-import torch.nn as nn
-import torch.optim as optim
-from PIL import Image 
-import csv
+import torch
+import os
+import torchvision.transforms as transforms
+from tqdm import notebook
 
-class tinyImageDataset:
 
-    def __init__(self, 
-                path, 
-                download=True, 
-                splitRatio=0.70, 
-                random_seed=110, 
-                transform=None):
-        self.path=path
-        self.download=download
-        self.split=split
-        self.random_seed=random_seed
-        self.transform=transform
 
-        # download the dataset
-        if download:
-            self.downloadDataset()
+def TinyImageNetDataSet(splitRatio = 70, test_transforms = None, train_transforms = None):
+  classes = extract_classnames(path = "tiny-imagenet-200/wnids.txt")
+  data = TinyImageNet(classes, defPath="tiny-imagenet-200")
+  traindata_len = len(data)*splitRatio//100
+  testdata_len = len(data) - traindata_len 
+  train, val = random_split(data, [traindata_len, testdata_len])
+  train_dataset = DatasetFromSubset(train, transform=train_transforms)
+  test_dataset = DatasetFromSubset(val, transform=test_transforms)
+
+  return train_dataset, test_dataset, classes
+
+
+# Extract class names out of wninds.txt
+def extract_classnames(path = "tiny-imagenet-200/wnids.txt"):
+  IDFile = open(path, "r")
+  classes = []
+  for line in IDFile:
+    classes.append(line.strip())
+  return classes
+
+
+
+# TinyImageNet class
+class TinyImageNet(Dataset):
+    def __init__(self, classes, defPath):
+        """
+        Custom Dataset Class with couple of helper funcs
+
+        Arguments:
+        classes: Dataset Classes
+        defPath: Common Path for Dataset Files
+        """
         
-        if splitRatio > 1:
-            raise ValueError("train_split must be less than 1")
+        self.classes = classes
+        self.defPath = defPath
+        self.data = []
+        self.target = []
+        
+        # Open Class ID File in read mode
+        wnids = open(f"{defPath}/wnids.txt", "r")
 
-        self._classID = self._mapID_to_className()  
-        self.data, self.target = self._loadData()
 
-        # shuffle the dataset
-        self._imageIndex = np.arange(self.target)
-        np.random.seed(random_seed)
-        np.random.shuffle(self._imageIndex)
+        # Training Part
+        trainImagePath = defPath+"/train/"
+        for cls in notebook.tqdm(wnids, total = 200):
+          cls = cls.strip() # strip spaces out of class names
+          indFolderPath = trainImagePath + cls + "/images/"
+          for i in os.listdir(indFolderPath):
+            img = Image.open(indFolderPath + i)
+            npimage = np.asarray(img)
+            
+            if(len(npimage.shape) == 2):
+              npimage = np.repeat(npimage[:, :, np.newaxis], 3, axis=2) # add a new dim using np.newaxis, if it's a 2D
+            
+            self.data.append(npimage)  
+            self.target.append(self.classes.index(cls))
 
-        # Split the data using Image indices
-        splitValue = int(len(self._imageIndex)*self.splitRatio) 
-        self._imageIndex = self._imageIndex[:splitValue]  if train else self._imageIndex[splitValue:]
-
+        # Validation Part
+        valAnntns = open(f"{defPath}/val/val_annotations.txt", "r")
+        for i in notebook.tqdm(valAnntns, total =10000):
+          img, cls = i.strip().split("\t")[:2]
+          img = Image.open(f"{defPath}/val/images/{img}")
+          npimage = np.asarray(img)
+          
+          if(len(npimage.shape) == 2):  
+                npimage = np.repeat(npimage[:, :, np.newaxis], 3, axis=2)
+          
+          self.data.append(npimage)  
+          self.target.append(self.classes.index(cls))
+            
 
     def __len__(self):
-        return len(self._imageIndex) 
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        data = self.data[idx]
+        target = self.target[idx]
+        img = data     
+        return data,target
+
+
+
+class DatasetFromSubset(Dataset):
+    def __init__(self, subset, transform=None):
+        """
+        DatasetFromSubset: Class for loading split data and transforming the same
+
+        Arguments:
+        subset: Split part of the Dataset
+        transform: List of transforms that needs to applied, by default None
+        """
+        self.subset = subset
+        self.transform = transform
 
 
     def __getitem__(self, index):
-        image_index = self._imageIndex[index]
-        image = self.data[image_index]   
-
+        x, y = self.subset[index]
         if self.transform:
-            image = self.transform(image)
+            x = self.transform(x)
+        return x, y
 
-        return image, self.target[image_index]
+    def __len__(self):
+        return len(self.subset)
 
-    
-    def __repr__(self):
-        head = 'TinyImageNet Dataset'
-        body = ['Number of datapoints: {}'.format(self.__len__())]
-        if self.path is not None:
-            body.append('Root Location: {}'.format(self.path))
-        body += [f'Split: {"Train" if self.train else "Test"}']
-        if hasattr(self, 'transforms') and self.transforms is not None:
-            body += [repr(self.transforms)]
-        lines = [head] + [' ' * 4 + line for line in body]
-        return '\n'.join(lines)
-
-
-    def tinyClasses(self):
-        return tuple(cls[1]['name']  for cls in sorted(self._classID, key=lambda x:x[1]['id']))     
-
-    def _mapID_to_className(self):
-        with open(os.path.join(self.path, 'wnids.txt')) as f:
-            class_ids = {x[:-1]: '' for x in f.readlines()} 
-
-        with open(os.path.join(self.path, 'words.txt')) as f:
-            class_id = 0
-            for line in csv.reader(f, delimiter='\t'):
-                if line[0] in class_ids:
-                    class_ids[line[0]] = {"name": line[1], "id": class_id}
-                    class_id += 1
-        
-        return class_ids 
-
-    def _loadImage(self, image_path):
-        img = Image(image_path)
-
-        if img.mode == 'L':
-            img_array = np.array(img) #convert image to np arra
-            img = np.stack((img_array,)*3, axis=-1)
-            img = Image.fromarray(img.astype('uint8'), 'RGB') #converting the np image back normal
-
-        return img
-    
-    def _loadData(self):
-        data = []
-        target = []
-
-        train_path = os.path.join(self.path, "train")
-        for classDir in os.listdir(train_path):
-            trainData_path = os.path.join(train_path, classDir)
-            for image in os.listdir(trainData_path):
-                if image.lower().endswith(".jpeg"):
-                    data.append(self._loadImage(trainData_path, image))
-                    target.append(self._classID[classDir]["id"])
-        
-
-        
-        val_path = os.path.join(self.path, 'val')
-        valImages_path = os.path.join(val_path, 'images')
-        with open(os.path.join(val_path, 'val_annotations.txt')) as f:
-            for line in csv.reader(f, delimiter='\t'):
-                data.append(self._loadImage(os.path.join(valImages_path, line[0])))
-                target.append(self._classID[line[1]]['id'])
-        
-        return data, target
-    
-    def downloadDataset(self):
-        if not os.path.exists(self.path):
-            r = requests.get('http://cs231n.stanford.edu/tiny-imagenet-200.zip', stream=True)
-            zip_ref = zipfile.ZipFile(BytesIO(r.content))
-            zip_ref.extractall(os.path.dirname(self.path))
-            zip_ref.close()
-
-            # Move file to appropriate location
-            os.rename(os.path.join(os.path.dirname(self.path), 'tiny-imagenet-200'), self.path)
-        else:
-            print('Files already downloaded.')
-
-def loader(self, train=True):
-        """Create data loader.
-        Args:
-            train (bool, optional): True for training data. (default: True)
-        
-        Returns:
-            Dataloader instance.
-        """
-
-        loader_args = {
-            'batch_size': 128
-            'num_workers': 4,
-            'pin_memory': True
-        }
-
-        return data_loader(
-            self.train_data, **loader_args
-        ) if train else data_loader(self.val_data, **loader_args)
-
-
-def data_loader(data, shuffle=True, batch_size=1, num_workers=1, cuda=False):
-    """Create data loader
-    Args:
-        data (torchvision.datasets): Downloaded dataset.
-        shuffle (bool, optional): If True, shuffle the dataset. 
-            (default: True)
-        batch_size (int, optional): Number of images to considered
-            in each batch. (default: 1)
-        num_workers (int, optional): How many subprocesses to use
-            for data loading. (default: 1)
-        cuda (bool, optional): True is GPU is available. (default: False)
-    
-    Returns:
-        DataLoader instance.
-    """
-
-    loader_args = {
-        'shuffle': shuffle,
-        'batch_size': batch_size
-    }
-
-    # If GPU exists
-    if cuda:
-        loader_args['num_workers'] = num_workers
-        loader_args['pin_memory'] = True
-    
-    return torch.utils.data.DataLoader(data, **loader_args)
