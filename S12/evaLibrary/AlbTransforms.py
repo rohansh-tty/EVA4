5cbd89c57ba23947f05bc9d3efcdcf8740e4e6fb
RohanShetty <shettyrohan641@yahoo.com>
2020-06-28 16:25:15 +0530

    Updated Transforms

--- S12/evaLibrary/AlbTransforms.py
+++ S12/evaLibrary/AlbTransforms.py
@@ -6,20 +6,15 @@ import numpy as np
 
 class AlbumentationTransforms:
   """
-  Helper class to add Transformations to Test & Train Data using Albumentations
+  Helper class to create test and train transforms using Albumentations
   """
   def __init__(self, transforms_list=[]):
-    """
-    Argument:
-    transforms_list: List of Transformations from Albumentation Lib
-    """
-    
-    transforms_list.append(AP.ToTensor()) # by default add ToTensor to conv the modified image to Tensor
+    transforms_list.append(AP.ToTensor()) 
     self.transforms = A.Compose(transforms_list)
 
 
-  def __call__(self, img): # this is to make the instance callable
-    img = np.array(img)
+  def __call__(self, img):
+    img = np.array(img) 
     return self.transforms(image=img)['image']
 
   
